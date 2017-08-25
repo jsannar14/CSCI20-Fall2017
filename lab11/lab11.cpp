5815dde79a6d6b330290f4114effcd829a4b5a24
John Sannar <jsannar003@student.butte.edu>
2017-08-25 02:11:56 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,35 @@
+/* **random number guessing game**
+
+1. computer select number between 1 and 500
+2. User must now pick a number between the set range (1 and 500)
+3. if user input is lower than computer selected number computer will say 'higher' and subtract 1 from score. 
+4. if user input is higher than computer selected number computer will say 'lower' and subtract 1 from score.
+5. repeat steps 3 and 4 until user guess is correct.
+5. if user is picks the correct number they win
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+    int main()
+    {
+        int x = 1;
+        int y = 500;
+        
+            cout << "Please pick a number between " << x << " and " << y;
+        
+        int i;
+        cout >> i;
+        
+        
+        
+        
+        
+    }
+    
+    
+    
