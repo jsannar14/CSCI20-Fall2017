5d818a49928e8f6f2b09e2b144fb6d09b4caf719
John Sannar <jsannar003@student.butte.edu>
2017-09-01 01:46:40 +0000

    Lab1.3 submission

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -1,5 +1,5 @@
 //Created By: John Sannar
-//Created On: 8/24/2016 
+//Created On: 8/31/2016 
 //A simple calculator for the human population in the United states 
  
  #include <iostream> 
@@ -7,8 +7,10 @@
  
  int main() 
  {
-    int populationCurrent =  325365189;
-    int populationGain = 2628334;
+    int populationCurrent =  325365189; 
+    int year = 31536000;
+    int netGain = 12; // Net gain of one person in seconds( Net gain of one person every "12" seconds)
+    int populationGain = year / netGain;
     int yearInput; 
     
      cout << "Enter the number of years you would like predict the human population of the United States (Ex. 99) "<< endl;
@@ -22,3 +24,8 @@
     
  }
  
+/* 
+Enter the number of years you would like predict the human population of the United States (Ex. 99) 
+99
+In the year 2116, the population will be 585570255 in the United States
+*/
