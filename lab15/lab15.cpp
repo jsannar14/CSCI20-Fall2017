61f096d4f52a7d20868fdbff24b4142ff0a40813
John Sannar <jsannar003@student.butte.edu>
2017-09-08 02:10:10 +0000

    Lab1.5 submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,56 @@
+//Created By: John Sannar
+//Created On: /7/2017
+//A madlib program designed to take user inputs and imbed them into a story
+ 
+
+#include <iostream> 
+#include <string>
+using namespace std;
+
+    int main() 
+    {
+  
+    string adjective = "Horror"; // starting here are the declered string variables that are set to a specific word but can be changed by user input.
+    string pluralNounOne = "Pizza";
+    string pluralNounTwo = "Dogs";
+    string pluralNounThree = "Poopy Diapers";
+    string nounOne = "Keyboard";
+    string typeOfLiquid = "Urine";
+    string exclaimation = "Woah!";
+    
+    
+        cout << "Please enter an adjective" << endl; // line that asks for user input
+        
+            cin >> adjective; // user input line.
+    
+        cout << "Please enter a plural noun" << endl;
+        
+            cin >> pluralNounOne;
+    
+        cout << "Please enter another plural noun" << endl;
+        
+            cin >> pluralNounTwo;
+    
+        cout << "Please enter one more plural noun" << endl;
+        
+            cin >> pluralNounThree;
+    
+        cout << "oh no wait, enter another plural noun" << endl;
+        
+            cin >> nounOne;
+        
+        cout << "Please enter a type of liquid" << endl;
+        
+            cin >> typeOfLiquid;
+        
+        cout << "Please enter an exclaimation (Example: Woah!)" << endl;
+        
+            cin >> exclaimation;
+    
+    
+    cout <<  "Children are fascinated by " << adjective << ". Stuff like that in stories is likely to scare the " << pluralNounOne << " out of them or make their " << pluralNounTwo << " stand on end. " << endl;
+    cout << "It is especially funny when they get so scared that their " << nounOne << " fills with " << typeOfLiquid << ". Most of the time the kids will scream " << exclaimation << " when they are scared." << endl;
+    cout << "No matter the age of the person, they fear " << pluralNounThree << ". Becuase everybody fears " << pluralNounThree << endl;
+
+    
+    }
